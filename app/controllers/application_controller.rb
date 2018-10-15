2e743948e1db38dc754beb73dd337d4dbf6c3232
Jan Czizikow <jan.czizikow@gmail.com>
2018-10-15 08:49:17 +0200

    Add pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@
 
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+  include Pundit
 
   respond_to :json
 
   before_action :authenticate_user!, unless: :devise_controller?
+  after_action :verify_authorized, unless: :devise_controller?
+  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
 end

824253a3affb0363afed0c1e1f1988db7e547061
Jan Czizikow <jan.czizikow@gmail.com>
2021-07-26 07:41:49 +0200

    fix user mailer spec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,16 +4,17 @@ require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
   describe 'welcome' do
-    let(:mail) { UserMailer.welcome }
+    let(:user) { build(:user) }
+    let(:mail) { UserMailer.welcome(user) }
 
     it 'renders the headers' do
-      expect(mail.subject).to eq('Welcome')
-      expect(mail.to).to eq(['to@example.org'])
-      expect(mail.from).to eq(['from@example.com'])
+      expect(mail.subject).to eq('Welcome to Xodoki')
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(['no-reply@ganelin.club'])
     end
 
     it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
+      expect(mail.body.encoded).to match('Welcome to Xodki')
     end
   end
 end
